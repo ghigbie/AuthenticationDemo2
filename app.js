db9f677aefc22d2f866b987873f82b37b6339bb8
ghigbie <gehigbie@gmail.com>
2017-04-18 21:22:36 +0000

    Add not found path

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express"),
       app = express();
       
+app.set("view engine", "ejs");
+      
+app.get("*", (req, res) => {
+    res.render("notfound");
+});
 
 app.listen(process.env.PORT, process.env.IP, () => {
   console.log("Server is listening");  
