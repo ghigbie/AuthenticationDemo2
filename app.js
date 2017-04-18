ee5993dcd6a105eada7bf6579a3b2a9c70bb9d72
ghigbie <gehigbie@gmail.com>
2017-04-18 21:34:28 +0000

    Add paths for app.js

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require("express"),
       app = express();
       
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+   res.render("home"); 
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/secret", (req, res) => {
+   res.render("secret"); 
+});
       
 app.get("*", (req, res) => {
     res.render("notfound");
